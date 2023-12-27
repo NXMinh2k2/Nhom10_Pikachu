da2377e7721799bb6f2c73b6bda2800026cfb173
khanhlyvu <99166148+khanhlyvu@users.noreply.github.com>
2023-12-27 20:22:07 +0700

    Delete saigame-sg11-fd3df68b735f directory

--- saigame-sg11-fd3df68b735f/Assets/_Data/Block/BlocksProfileSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Data", menuName = "SO/BlocksProfile", order = 1)]
-public class BlocksProfileSO : ScriptableObject
-{
-    public List<Sprite> sprites = new List<Sprite>();
-}
--- saigame-sg11-fd3df68b735f/Assets/_Data/Node/NodeDirections.cs
+++
@@ -1,9 +0,0 @@
-
-public enum NodeDirections
-{
-    noDirection = 0,
-    up = 1,
-    right = 2,
-    down = 3,
-    left = 4,
-}
